b5f0ce8812c897f536a61c45c68f0fcfcd9b32cf
Sagit Bolat <sagitbolat@gmail.com>
2023-08-21 16:52:33 -0700

    statically linked std libs

--- build.bat
+++ build.bat
@@ -8,15 +8,15 @@ if "%3" == "-gl"    (GOTO CompileGL)
 if "%3" == "-al"    (GOTO CompileAL)
 
 :CompileUI
-    g++ -c -static Engine\UI\sky_ui_imgui.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -o sky_ui_imgui.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 
+    g++ -c Engine\UI\sky_ui_imgui.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -o sky_ui_imgui.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -static-libgcc -static-libstdc++ 
     move .\sky_ui_imgui.o .\%1\build_win
     if "%3" == "-f" (GOTO CompileGL) else (GOTO CompileGame)
 :CompileGL
-    g++ -c -static Engine\OpenGL_renderer2D.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -o OpenGL_renderer2D.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 
+    g++ -c Engine\OpenGL_renderer2D.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -o OpenGL_renderer2D.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -static-libgcc -static-libstdc++ 
     move .\OpenGL_renderer2D.o .\%1\build_win
     if "%3" == "-f" (GOTO CompileAL) else (GOTO CompileGame)
 :CompileAL
-    g++ -c -static Engine\OpenAL_sound.cpp -IC:\SDL2\include -IC:\glew\include -IC:\OpenAL_SDK\include -o OpenAL_sound.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -lOpenAL32 
+    g++ -c Engine\OpenAL_sound.cpp -IC:\SDL2\include -IC:\glew\include -IC:\OpenAL_SDK\include -o OpenAL_sound.o -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -lOpenAL32 -static-libgcc -static-libstdc++ 
     move .\OpenAL_sound.o .\%1\build_win
 
 :CompileGame
@@ -24,6 +24,6 @@ XCOPY Engine\assets\startup.png %1\build_win /D /Y
 XCOPY Engine\assets\glew32.dll %1\build_win /D /Y 
 XCOPY Engine\assets\SDL2.dll %1\build_win /D /Y 
 pushd %1\build_win
-g++ OpenGL_renderer2D.o OpenAL_sound.o sky_ui_imgui.o ..\%2.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -IC:\OpenAL_SDK\include -LC:\SDL2\lib -LC:\glew\lib -LC:\OpenAL_SDK\libs\Win64 -Wall -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -lOpenAL32 -o %2.exe 
+g++ OpenGL_renderer2D.o OpenAL_sound.o sky_ui_imgui.o ..\%2.cpp -IC:\SDL2\include -IC:\glew\include -IC:\glm -IC:\OpenAL_SDK\include -LC:\SDL2\lib -LC:\glew\lib -LC:\OpenAL_SDK\libs\Win64 -Wall -lmingw32 -lSDL2main -lSDL2 -lglew32 -lopengl32 -lOpenAL32 -static-libgcc -static-libstdc++ -o %2.exe 
 .\%2.exe
 popd
